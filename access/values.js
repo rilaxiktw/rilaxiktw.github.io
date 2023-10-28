dae996f4442284d8c7dce6725ddcbb88ba243f0f
rilaxiktw <149256077+rilaxiktw@users.noreply.github.com>
2023-10-28 16:28:44 +0100

    qwe

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "22.10.2005"; // Дата рождения
+var fullname = "Гнєвишева Катерина Сергіївна" // ФИО
+var name = "Катерина" // Имя
+
+// Паспорт
+var passport_id = "005439073" // Номер паспорта
+
+// КПП
+var kpp_id = "8463895743" // ИНН
+
+// Права
+var rights_valid_until = "01.01.2022" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "01.01.202" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
